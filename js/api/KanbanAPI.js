e5c9173f13321058422278a4a845c7c5d54c8564
João Victor <jvfernandes.contato@gmail.com>
2023-05-10 03:53:51 -0300

    feat: creation some functions in js script

---
+++ js/api/KanbanAPI.js
@@ -0,0 +1,74 @@
+// este arquivo vai conter o código para os componentes de armazenamento local;
+// esta classe está sendo exportada, o que irá permitir que importemos ela posteriormente em outros arquivos js;
+// esta classe irá conter diversos métodos estáticos ou funções para interagir com o armazenamento local;
+export default class KanbanAPI {
+  // este método será responsável por obter todos os cards dentro de uma lista específica;
+  static getItems(columnId) {
+    // cada um desses id's se referem ao id de uma das colunas da matriz retornada na função "read()";
+    const column = read().find(column => column.id == columnId);
+
+    // aqui estamos dizendo que se não for encontrada nenhuma coluna então retornamos um array vazio;
+    // outra alternativa seria lançar um erro;
+    if (!column) {
+      return [];
+    }
+
+    // se estiver tudo bem (ou seja, encontrarmos a coluna) então... retornamos essa coluna, recuperando os cards que estão dentro dela;
+    return column.items; 
+  }
+
+  // este método vai ser responsável por adicionar um novo card;
+  // ao inserir um card, precisamos saber qual o id da lista e qual o conteúdo do card;
+  static inserItem(columnId, content) {
+    const data = read();
+    // aqui estamos dizendo que a coluna vai ser igual a coluna com id igual ao que está sendo passado como parametro;
+    const column = data.find(column => column.id == columnId);
+    // aqui estamos declarando uma constante item, que se trata do card, que vai receber como propriedades um id e o conteúdo (content);
+    const item = {
+      id: Math.floor(Math.random() * 100000),
+      content
+    };
+
+    // aqui estamos verificando se a coluna existe;
+    if (!column) {
+      throw new Error("Column does not exist.");
+    }
+
+    // esta coluna é uma referência a uma matriz dentro da constante data;
+    column.items.push(item);
+    save(data);
+
+    return item;
+  }
+}
+
+function read() {
+  const json = localStorage.getItem("kanban-data");
+
+  // isto aqui vai rodar quando o usuário estiver carregando o quadro kanban pela pela primeira vez;
+  if (!json) {
+    return [
+      {
+        id: 1,
+        items: []
+      },
+      {
+        id: 2,
+        items: []
+      },
+      {
+        id: 3,
+        items: []
+      },
+    ]
+  }
+
+  // já isto aqui vai rodar quando o usuário estiver retornando para a sessão deles;
+  return JSON.parse(json);
+}
+
+// esta função vai receber novos dados;
+// estes dados são básicamente o que a função "read()" retorna;
+function save(data) {
+  localStorage.setItem("kanban-data", JSON.stringify(data));
+}
---
+++ js/main.js
@@ -0,0 +1,3 @@
+import KanbanAPI from "./api/KanbanAPI.js";
+
+console.log(KanbanAPI.inserItem(2, "I am new!"));
