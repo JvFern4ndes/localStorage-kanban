8ca8c653dfd62dddb50f21f6639ec94aa0eadd49
João Victor <jvfernandes.contato@gmail.com>
2023-05-10 10:55:38 -0300

    feat: create Kanban.js script

---
+++ js/view/Kanban.js
@@ -0,0 +1,36 @@
+// esta classe exportada será responsável por renderizar o kanban na interface, básicamente;
+export default class Kanban {
+  
+  // este construtor está fazendo referência à div kanban do html;
+  constructor(root) {
+    // aqui estamos mantendo a referência;
+    this.root = root;
+
+    // aqui estamos dizendo que para cada coluna, será criada uma instancia de coluna;
+    Kanban.columns().forEach(column => {
+
+    });
+  }
+
+  // aqui estamos definindo um método estatico chamado colunas, que será responsável por renderizar as listas na interface (irá retornar uma matriz para cada coluna);
+  static columns() {
+    return [
+      {
+        id: 1,
+        title: "Candidaturas"
+      },
+      {
+        id: 2,
+        title: "Teste Online / Dinâmica"
+      },
+      {
+        id: 3,
+        title: "Entrevista"
+      },
+      {
+        id: 4,
+        title: "Aguardando"
+      },
+    ];
+  }
+}
