376c32e9af6c853c39cc9e27e5bad624dd4e6c86
João Victor <jvfernandes.contato@gmail.com>
2023-05-11 06:49:11 -0300

    feat: implementation of update cards function

--- js/view/Item.js
+++ js/view/Item.js
@@ -1,3 +1,5 @@
+import KanbanAPI from "../api/KanbanAPI.js";
+
 // aqui estamos exportando a classe padrão do Item;
 export default class Item {
   // o construtor vai passar pelo id e pelo content do item;
@@ -14,6 +16,26 @@ export default class Item {
 
     // aqui estamos definindo uma referencia para o content, que será usada para situações de atualizações do content;
     this.content = content;
+
+    // aqui estamos implementando a função responsável por editar um card;
+    const onBlur = () => {
+      const newContent = this.elements.input.textContent.trim();
+
+      // aqui estamos dizendo que se o novo conteúdo for igual ao atual, para não fazer nada, apenas retornar;
+      if (newContent == this.content) {
+        return;
+      }
+
+      // caso contrário, atualizamos o conteúdo;
+      this.content = newContent;
+      // chamando o método update da API, e passando para ele o id do construtor e o objeto do novo conteúdo;
+      KanbanAPI.updateItem(id, {
+        content: this.content
+      })
+    };
+
+    // aqui estamos adicionando um ouvidor de eventos, onde o evento a ser ouvido é o desfoque, e a função a ser chamada a onBlur();
+    this.elements.input.addEventListener("blur", onBlur);
   }
 
   // esta aqui é a raiz para a criação de um novo item específico;
